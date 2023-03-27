55bc6c13232ce7b3715bc9001b71f8e855d54cb9
iroj <iroj@fresnoU.org>
2023-03-27 10:14:46 -0700

    updated commit

--- Arrays1.js
+++ Arrays1.js
@@ -20,3 +20,21 @@
 // checkEmpty(empty)
 
 //4
+function reverseArray(arr){
+    let lastIndex = arr.length-1;
+    for (let i=lastIndex; i>=0; i--){
+        console.log(arr[i])
+    }
+}
+reverseArray([20, 10, 5, 1]);
+reverseArray(['a', 'b', 'c', 'd', 'e'])
+
+//5
+function everyThird(arr){
+for (let i = 2; i<arr.length; i=i+3){
+    console.log(arr[i])
+    }
+}
+everyThird([10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130]);
+
+//6
